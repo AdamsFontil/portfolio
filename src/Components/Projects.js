8a4158d71b5413a867b2f9ba22ff452ba55baeb8
Adams Fontil <adamsfuntil@gmail.com>
2023-05-01 09:15:37 -0400

    fix projects section and add readme

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -2,7 +2,19 @@ import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons'
 import { faGithub } from '@fortawesome/free-brands-svg-icons'
-import Image1 from '../Assets/pexels-ozan-çulha-16337978 (1).jpg'
+import Slider from 'react-slick'
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
+
+
+const images = {}
+function importAll(r) {
+  r.keys().forEach(key => images[key] = r(key))
+}
+// eslint-disable-next-line no-undef
+importAll(require.context('../Assets/projectPics', false, /\.(png|jpe?g|svg)$/))
+
+console.log('images', images)
 
 const projectsData = [
   {
@@ -11,62 +23,98 @@ const projectsData = [
     github: 'https://github.com/',
     live: 'site.com',
     description: 'Publix app created to test both design skills, and backend. This site has authentication, and uses mongoDB to store those users, and the items they have in their carts',
-    screenshot: Image1
-  },
-  {
-    name: 'Calculator',
-    tools: ['javascript', 'css'],
-    github: '',
-    live: '$69.99',
-    description: 'Calculator app thats just like the one on your phone. This was one my first big projects, testing my understanding javascript. Made with vanilla javascript and css',
-    screenshot: Image1
-  },
-  {
-    name: 'Todo List',
-    tools: ['javascript', 'css'],
-    github: '',
-    live: '$69.99',
-    description: 'Another plain old javascript project. In this project I made use objects, and protypal inheritance',
-    screenshot: Image1
-  },
-  {
-    name: 'Library Project',
-    tools: ['javascript', 'css'],
-    github: '24 Hours Advance Notice Required',
-    live: '$69.99',
-    description: 'This project let us add books to your collection when youre done reading them. This site made use of form validation, factory functions, inheritance and properly structuring code.',
-    screenshot: Image1
-  },
-  {
-    name: 'PhoneBook',
-    tools: ['react', 'mongoDB','express'],
-    github: '24 Hours Advance Notice Required',
-    live: '$69.99',
-    description: 'FullStack Project that I made as I went through the FullStack Open cirriculum. It stores phone numbers in mongoDB, allows users and authenticaticates them ',
-    screenshot: Image1
+    imageName: 'publixClonePic'
   },
   {
     name: 'Blog List',
     tools: ['react', 'mongoDB','express', 'cypress', 'jest'],
-    github: '24 Hours Advance Notice Required',
+    github: 'https://github.com/AdamsFontil/fsoPart4/tree/gh-pages',
     live: '$69.99',
     description: 'This is yet another fullStack project so it has a frontend made with react, backend using Rest APIs and express, and it also has database with mongoDB that stores users and their blogs. Users can delete and like blogs. This project uses testing with Jest for unit test, and Cypress for E2E integration testing.',
-    screenshot: Image1
-  }
+    imageName: 'blogPic'
+  },
+  {
+    name: 'Personal Website',
+    tools: ['react', 'tailwindcss'],
+    github: '',
+    live: '$69.99',
+    description: 'Personal website made mainly with react and tailwind to showcase my abilities and the projects that Ive made',
+    imageName: 'portfolioPic'
+  },
+  {
+    name: 'Library Project',
+    tools: ['javascript', 'css'],
+    github: 'https://github.com/AdamsFontil/library',
+    live: 'https://adamsfontil.github.io/library/',
+    description: 'The Library App is a user-friendly web application that allows users to add, remove, and toggle read status of books in their collection. It features form validation, factory functions, and inheritance, and ensures that books are not duplicated in the library. The app is well-organized and has a responsive design, and uses local storage to save data.',
+    imageName: 'libraryPic'
+  },
+  {
+    name: 'PhoneBook',
+    tools: ['react', 'mongoDB','express'],
+    github: 'https://github.com/AdamsFontil/fsoPart3',
+    live: 'https://twilight-rain-3479.fly.dev/',
+    description: 'The PhoneBook project is part of the FullStack Open curriculum. It is a full-stack project that allows users to add and manage phone numbers in a MongoDB database. The project features user authentication using JWT (JSON Web Tokens), which allows users to log in and access their phone numbers. The project is built using React, MongoDB, and Express, and it demonstrates proficiency in these technologies. ',
+    imageName: 'phonebookPic'
+  },
+  // {
+  //   name: 'Todo List',
+  //   tools: ['javascript', 'css'],
+  //   github: '',
+  //   live: '$69.99',
+  //   description: 'Calculator app thats just like the one on your phone. This was one my first big projects, testing my understanding javascript. Made with vanilla javascript and css',
+  //   imageName: Image1
+  // },
+  // {
+  //   name: 'Calculator',
+  //   tools: ['javascript', 'css'],
+  //   github: '',
+  //   live: '$69.99',
+  //   description: 'Calculator app thats just like the one on your phone. This was one my first big projects, testing my understanding javascript. Made with vanilla javascript and css',
+  //   imageName: Image1
+  // },
 ]
 
 
 
+
+
 const Projects = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  }
+
   return (
-    <div className='p-4 flex flex-col gap-2'>
-      <h1 className='text-4xl'>My Projects</h1>
-      <div className='grid grid-cols-1 gap-3 md:grid-cols-3'>
+    <div className='p-4 bg-orange-400 text-blue-600 border-2 border-white rounded-lg'>
+      <h1 className='text-6xl mb-5'>My Projects</h1>
+      <Slider className='projects-slider mx-8 mb-6' {...settings}>
         {projectsData.map((project, index) => (
-          <div className=' border-orange-400 rounded-lg border-2 text-white' key={index}>
-            <img className='w-50 object-contain border-2 ' src={project.screenshot} alt={project.name} />
-            <div className='p-4'>
-              <div className='flex items-center justify-between text-orange-400'>
+          <div className=' text-white' key={index}>
+            <img className='w-49 object-contain' src={images[`./${project.imageName}.png`]} alt={project.name} />
+            <div className='pt-4'>
+              <div className='flex items-center justify-between text-blue-600'>
                 <p className=''>{project.name}</p>
                 <div className='flex gap-2 justify-end'>
                   <a href={project.github} target='_blank' rel="noreferrer" ><FontAwesomeIcon icon={faGithub} className='hover:text-white'  /> </a>
@@ -75,18 +123,12 @@ const Projects = () => {
               </div >
               <div className='flex flex-col gap-3'>
                 <p>{project.description}</p>
-                {/* <div>
-                  <h2>Tools used:</h2>
-                  <p>{project.tools}</p> <br></br>
-                </div> */}
               </div>
-
             </div>
           </div>
         ))}
-      </div>
+      </Slider>
     </div>
-
   )
 }
 
